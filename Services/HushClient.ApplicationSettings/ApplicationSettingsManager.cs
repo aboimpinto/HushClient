8a15fe8b7ba2500a698a812590bdde942a586c0c
Paulo Aboim Pinto <aboimpinto@gmail.com>
2024-03-04 22:31:32 +0100

    Update ApplicationSettings to netstandard2.1

--- Services/HushClient.ApplicationSettings/ApplicationSettingsManager.cs
+++ Services/HushClient.ApplicationSettings/ApplicationSettingsManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Reactive.Subjects;
+﻿using System.IO;
+using System.Reactive.Subjects;
 using System.Reflection;
+using System.Threading.Tasks;
 using HushClient.ApplicationSettings.Model;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
